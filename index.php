dedff8ad680455e79a8bf912ffe0f2f9f2fdd271
pixelaner <pixelaner@googlemail.com>
2015-03-05 09:44:52 +0100

    added wordpress files

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>GH-Test</title>
-</head>
-<body>
-	<h1>GH-Test</h1>
-</body>
-</html>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
